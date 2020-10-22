490ee56266fef71965cc8482f17b5203b78b8bc8
AmirGhan <amir.ghandeharioon@gmail.com>
2020-10-21 23:44:31 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 //our root app component
-import { Component, NgModule, VERSION, AfterViewInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  NgModule,
+  VERSION,
+  AfterViewInit,
+  ViewChild
+} from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
@@ -19,14 +25,14 @@ export class AppComponent {
   birthdayYear: number;
   itemNumber: number;
 
-  @ViewChild('f') form: any;
+  @ViewChild("f") form: any;
 
   constructor(private modalService: NgbModal) {
     this._users = [
-      new User("Ali", "Delshad", 1, { day: 1, month: 1, year: 1991 }),
-      new User("Hamid", "Sadeghi", 2, { day: 2, month: 2, year: 1992 }),
-      new User("Amir", "Olfat", 3, { day: 3, month: 3, year: 1993 }),
-      new User("Keyvan", "Nasr", 4, { day: 4, month: 4, year: 1994 })
+      new User("Ali", "Delshad", 12, { day: 10, month: 21, year: 1991 }),
+      new User("Hamid", "Sadeghi", 58, { day: 14, month: 5, year: 1992 }),
+      new User("Amir", "Olfat", 7, { day: 8, month: 12, year: 1993 }),
+      new User("Keyvan", "Nasr", 24, { day: 29, month: 9, year: 1994 })
     ];
   }
 
@@ -51,14 +57,14 @@ export class AppComponent {
   edit(): void {
     if (this.form.valid) {
       this._users[this.editingIndex] = new User(
-      this.name,
-      this.family,
-      this.itemNumber,
-      {
-        day: this.birthdayDay,
-        month: this.birthdayMonth,
-        year: this.birthdayYear
-      }
+        this.name,
+        this.family,
+        this.itemNumber,
+        {
+          day: this.birthdayDay,
+          month: this.birthdayMonth,
+          year: this.birthdayYear
+        }
       );
       this.editingIndex = undefined;
       this.name = "";
@@ -68,25 +74,24 @@ export class AppComponent {
       this.birthdayYear = undefined;
       this.itemNumber = undefined;
     }
-
   }
 
   add(): void {
-        if (this.form.valid) {
-          this._users.push(new User(this.name, this.family, this.itemNumber, {
-            day: this.birthdayDay,
-            month: this.birthdayMonth,
-            year: this.birthdayYear
-          }));
-          this.name = "";
-          this.family = "";
-          this.birthdayDay = undefined;
-          this.birthdayMonth = undefined;
-          this.birthdayYear = undefined;
-          this.itemNumber = undefined;
-
-        }
-
+    if (this.form.valid) {
+      this._users.push(
+        new User(this.name, this.family, this.itemNumber, {
+          day: this.birthdayDay,
+          month: this.birthdayMonth,
+          year: this.birthdayYear
+        })
+      );
+      this.name = "";
+      this.family = "";
+      this.birthdayDay = undefined;
+      this.birthdayMonth = undefined;
+      this.birthdayYear = undefined;
+      this.itemNumber = undefined;
+    }
   }
 
   cancel(): void {
